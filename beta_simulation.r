7456162ff530e43c4386272c0dfc27aec37843ae
Parsa Hosseini <parsahosseini2001@gmail.com>
2024-07-17 13:31:56 +0330

    Add power function calculation to Beta simulation

--- beta_simulation.r
+++ beta_simulation.r
@@ -1,12 +1,17 @@
 source("statistic.r")
 
+# defining parameters
 n <- 20
 r <- 1
 m <- 1
 alpha <- .05
 theta_0 <- 8
 theta_1 <- 3
+
+# calculating critical value
 cv <- critical_value(theta_0, theta_1, r, n, m, alpha)
+
+# performing NHST
 replicates <- 10000
 p <- numeric(replicates)
 for (i in 1:replicates) {
@@ -16,4 +21,15 @@ for (i in 1:replicates) {
 }
 p_hat <- mean(p)
 se_hat <- sqrt(p_hat * (1 - p_hat) / replicates)
-print(c(p_hat, se_hat))
+
+# caculating power function
+n2 <- 1000
+power <- 0
+for (i in 1:n2) {
+  x <- rbeta(n, shape1 = theta_1, shape2 = 1)
+  d <- sum(abs(x))
+  power <- if (is_in_rejection_region(d, cv, r)) power + 1 else power
+}
+power <- power / n2
+
+print(c(p_hat, se_hat, power))
