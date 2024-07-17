af7ad08e87af6121f11ef1e6e257d26351e198f4
Parsa Hosseini <parsahosseini2001@gmail.com>
2024-07-17 13:29:01 +0330

    Add power function calculation to Gamma simulation

--- gamma_simulation.r
+++ gamma_simulation.r
@@ -1,5 +1,6 @@
 source("statistic.r")
 
+# defining parameters
 n <- 20
 r <- -1
 beta <- 2
@@ -7,15 +8,28 @@ m <- -1 * beta
 alpha <- .05
 theta_0 <- 8
 theta_1 <- 3
+
+# calculating critical value
 cv <- critical_value(theta_0, theta_1, r, n, m, alpha)
+
 replicates <- 10000
 p <- numeric(replicates)
 for (i in 1:replicates) {
-  #print(i)
   x <- rgamma(n, beta, scale = theta_0)
   d <- sum(x)
   p[i] <- is_in_rejection_region(d, cv, r)
 }
 p_hat <- mean(p)
 se_hat <- sqrt(p_hat * (1 - p_hat) / replicates)
-print(c(p_hat, se_hat))
+
+# caculating power function
+n2 <- 1000
+power <- 0
+for (i in 1:n2) {
+  x <- rgamma(n, beta, scale = theta_1)
+  d <- sum(abs(x))
+  power <- if (is_in_rejection_region(d, cv, r)) power + 1 else power
+}
+power <- power / n2
+
+print(c(p_hat, se_hat, power))
